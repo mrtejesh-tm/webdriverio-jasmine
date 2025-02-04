a73c57da0694264245f8158afa9b4a6bcc108eda
mrtejesh-tm <rudraraju.tejesh@testingmavens.com>
2025-02-04 19:29:49 +0530

    added constants file for screentexts

---
+++ test/pageobjects/login/constants.js
@@ -0,0 +1,9 @@
+export const LoginScreenText = {
+    CreateAccountText: 'Create an Account',
+    CreateAccountMessage: 'Join our community',
+    WelcomeText: 'Welcome Back',
+    WelcomeMessage: 'Login to your account',
+    Username: 'Email Address',
+    Password: 'Password',
+    LoginButton: 'Login',
+}
