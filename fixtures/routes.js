e3d7cc8ade00692b42aeb360281ea1a49a7143d2
mrtejesh-tm <rudraraju.tejesh@testingmavens.com>
2025-02-04 18:12:58 +0530

    fixtures update

---
+++ fixtures/routes.js
@@ -0,0 +1,12 @@
+export const routes = {
+    Home: '',
+    Login: 'signin',
+    Signup: 'signup',
+    Shop: 'shop',
+    Component: 'components',
+    About: 'about',
+    Journal: 'journal',
+    API: 'apidoc',
+    Contact: 'contact',
+    Cart: 'cart',
+}
